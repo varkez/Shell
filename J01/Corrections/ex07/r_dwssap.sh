ea5ee961488c5914fa6ddc129c75b68bc9d58e29
varkez <cariou.franck@gmail.com>
2018-04-18 22:29:06 +0200

    J01 de la Piscine

---
+++ J01/Corrections/ex07/r_dwssap.sh
@@ -0,0 +1,1 @@
+cat /etc/passwd | grep -v \# | sed -e 's/:.*//g' | sed -n 'n;p' | rev | sort -r | awk 'NR >= ENVIRON["FT_LINE1"] && NR <= ENVIRON["FT_LINE2"]' | tr "\n" "," | sed 's/,/, /g' | sed 's/, $/./' | tr -d '/n'
